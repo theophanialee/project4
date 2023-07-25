f56355c59698067a9729fd389901cbf6760c473a
Theophania <pha.randomacc@gmail.com>
2023-07-25 11:04:22 +0800

    EditProfile

--- twitter2.0/src/components/settings/EditUser.jsx
+++ twitter2.0/src/components/settings/EditUser.jsx
@@ -1,35 +1,35 @@
 import { useContext, useRef, useState } from "react";
 import ".././form.css";
 import { UserContext } from "../../pages/App/App";
-
-// You may need to import other utility functions for profile picture upload and date of birth update
+import sendRequest from "../../utilities/send-request";
 
 export default function EditUser() {
   const [errorMsg, setErrorMsg] = useState(null);
-  const [displayName, setDisplayName] = useState(""); // State for display name input
-  const [profilePicture, setProfilePicture] = useState(null); // State for profile picture file
-  const [dateOfBirth, setDateOfBirth] = useState(""); // State for date of birth input
-  const [profileDescription, setProfileDescription] = useState(""); // State for profile description input
-
+  const [displayName, setDisplayName] = useState("");
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [profileDescription, setProfileDescription] = useState("");
   const { user } = useContext(UserContext);
 
-  // Function to handle submit form
-  async function handleSubmit(e) {
+  async function handleEditProfile(e) {
     e.preventDefault();
-
-    // You can use the states (displayName, profilePicture, dateOfBirth, profileDescription) to update the user's information
-    // For demonstration purposes, let's just log the information
     console.log("Display Name:", displayName);
     console.log("Profile Picture:", profilePicture);
     console.log("Date of Birth:", dateOfBirth);
     console.log("Profile Description:", profileDescription);
-
-    setErrorMsg(null); // Reset error message
+    setErrorMsg(null);
+    try {
+      const response = await sendRequest(`api/profiles/editProfile`, "PATCH");
+      console.log(response);
+      setErrorMsg(null);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <>
-      <form className="form-container" onSubmit={handleSubmit}>
+      <form className="form-container" onSubmit={handleEditProfile}>
         <h1 className="text-center">Edit User</h1>
         <div className="flex flex-col">
           <label className="mb-1 mt-5">Display Name</label>
@@ -67,7 +67,7 @@ export default function EditUser() {
             rows="4"
             value={profileDescription}
             onChange={(e) => setProfileDescription(e.target.value)}
-            maxLength={500} // Set maximum character limit for the profile description
+            maxLength={300}
           />
         </div>
         <button className="bg-purple-500 hover:bg-purple-700 m-5">
